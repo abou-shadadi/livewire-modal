4116ce9abe7d6c6fc2c05cd66e3e2d2780804134
Abou-Shadadi <aboushadadadi@gmail.com>
2024-10-22 23:10:23 +0200

    Add files

--- resources/assets/js/script.js
+++ resources/assets/js/script.js
@@ -57,7 +57,12 @@ function _livewireModal() {
                 modalInstance.show();
             }
 
-            Livewire.emitTo('base-wire-modal', 'initModal', event.detail.modal, event.detail.args);            
+            Livewire.dispatch('initModal', {
+                component: 'base-wire-modal',
+                modal: event.detail.modal,
+                args: event.detail.args
+            });
+                     
         }
     };
 }
