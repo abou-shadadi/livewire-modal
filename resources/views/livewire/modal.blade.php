84cf06e0869f1302872a0477238d0be75bca4a9c
Abou-Shadadi <aboushadadadi@gmail.com>
2024-10-25 15:25:40 +0200

    Fix issue

--- resources/views/livewire/modal.blade.php
+++ resources/views/livewire/modal.blade.php
@@ -1,5 +1,12 @@
-<div wire:key="x--modal-zone{{$activeModal}}">
-    @if($activeModal)
-        @livewire($activeModal, $args, key($activeModal))
-    @endif
+<div>
+    @php
+        $key = Str::uuid() . '-' . $activeModal;
+    @endphp
+    <div wire:key="x--modal-zone-{{ $key }}">
+        @if ($activeModal)
+        @livewire($activeModal, $args, key($key))
+        @else
+            <p>No active modal</p>
+        @endif
+    </div>
 </div>
