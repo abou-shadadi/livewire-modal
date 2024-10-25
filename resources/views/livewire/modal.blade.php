3c0b70d32669834d314920f17bc57c13818c8e3a
Abou-Shadadi <aboushadadadi@gmail.com>
2024-10-25 14:31:23 +0200

    Fix issue

--- resources/views/livewire/modal.blade.php
+++ resources/views/livewire/modal.blade.php
@@ -1,10 +1,14 @@
-<div>
+<<div>
     @php
         $key = Str::uuid() . '-' . $activeModal;
     @endphp
     <div wire:key="x--modal-zone-{{ $key }}">
         @if ($activeModal)
-            <livewire:is :component="$activeModal" :args="$args" :key="$key" />
+            <livewire:is :component="$activeModal" :key="$key"
+                @foreach ($args as $argKey => $argValue)
+                    :{{ $argKey }}="$argValue"
+                @endforeach
+            />
         @else
             <p>No active modal</p>
         @endif
